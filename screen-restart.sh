c9b25c54923c42e1ea952f4cd06377273bfe6e73
devender.mishra <devender.mishra@ril.com>
2016-02-24 12:44:22 +0530

    Add script to restart screen

---
+++ screen-restart.sh
@@ -0,0 +1,16 @@
+#Now restart all the screens
+screen -S stack -p 18 -X stuff `echo '^C^[[A\n'`
+screen -S stack -p 'n-api' -X stuff `echo '^C^[[A\n'`
+screen -S stack -p 'n-cond' -X stuff `echo '^C^[[A\n'`
+screen -S stack -p 'n-crt' -X stuff `echo '^C^[[A\n'`
+screen -S stack -p 'n-net' -X stuff `echo '^C^[[A\n'`
+screen -S stack -p 'n-sch' -X stuff `echo '^C^[[A\n'`
+screen -S stack -p 'n-novnc' -X stuff `echo '^C^[[A\n'`
+screen -S stack -p 'c-api' -X stuff `echo '^C^[[A\n'`
+screen -S stack -p 'c-sch' -X stuff `echo '^C^[[A\n'`
+screen -S stack -p 'c-vol' -X stuff `echo '^C^[[A\n'`
+screen -S stack -p 'g-api' -X stuff `echo '^C^[[A\n'`
+screen -S stack -p 'g-reg' -X stuff `echo '^C^[[A\n'`
+screen -S stack -p 'ec2-api' -X stuff `echo '^C^[[A\n'`
+screen -S stack -p 'ec2-api-metadata' -X stuff `echo '^C^[[A\n'`
+screen -S stack -p 'ec2-api-s3' -X stuff `echo '^C^[[A\n'`
